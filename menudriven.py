d599cd7884a1683f48048ec072513f5e1e474ce0
Anusha Gupta <97082090+1906539@users.noreply.github.com>
2022-02-23 16:53:41 +0530

    Update menudriven.py

--- menudriven.py
+++ menudriven.py
@@ -1,1 +1,14 @@
-
+x=input("Enter the first number:")
+y=input("Enter the second number:")
+x=int(x)
+y=int(y)
+print("a=x+y\nb=x-y\nc=x/y\nd=x*y")
+s=input("Your Choice:")
+if s=='a':
+ print("Sum is:",x+y)
+elif s=='b':
+ print("Answer is:",x-y)
+elif s=='c':
+ print("Answer is:",x/y)
+elif s=='d':
+ print("Answer is:",x*y)
